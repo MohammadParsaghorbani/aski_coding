f6ac50794c504c5fad39aff3befdbbf038839f6c
MohammadParsaghorbani <mparsagh221@gmail.com>
2024-08-28 22:19:02 +0330

    Create read_aski.py

---
+++ read_aski.py
@@ -0,0 +1,16 @@
+line = int(input("line = "))
+file = open("aski.txt" , "r")
+ordd = []
+chr = []
+correct = ""
+content = file.readline()
+for i in range(line):
+    content = file.readline()
+for i in content:
+    x = ord(i)-11
+    ordd.append(x)
+for i in ordd:
+    correct += chr(i)
+print(content)
+print(correct)
+file.close()
